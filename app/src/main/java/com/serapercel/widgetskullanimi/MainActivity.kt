79b29df7db3a57f81eb07945d7b71f973a3b0d78
Serap <ercelserap@gmail.com>
2022-10-08 21:35:41 +0300

    Add checkBox and radioButton

--- app/src/main/java/com/serapercel/widgetskullanimi/MainActivity.kt
+++ app/src/main/java/com/serapercel/widgetskullanimi/MainActivity.kt
@@ -27,6 +27,8 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         }
+
+        // Switch
         // v - switch'in kendisi, b - switch'in durumu (true/false)
         binding.switch1.setOnCheckedChangeListener { v, b ->
             if (b) {
@@ -36,8 +38,42 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // Check Box
+        // v - checkBoxKotlin'in kendisi, b - checkBoxKotlin'in durumu (true/false)
+        binding.checkBoxKotlin.setOnCheckedChangeListener { v, b ->
+            if (b) {
+                Log.e("widgets", "Kotlin Seçildi")
+            }
+        }
+
+        // v - checkBoxJava'in kendisi, b - checkBoxJava'in durumu (true/false)
+        binding.checkBoxJava.setOnCheckedChangeListener { v, b ->
+            if (b) {
+                Log.e("widgets", "Java Seçildi")
+            }
+        }
+
+        // Radio Button
+        // v - radioButtonBarcelona'in kendisi, b - radioButtonBarcelona'in durumu (true/false)
+        binding.radioButtonBarcelona.setOnCheckedChangeListener { v, b ->
+            if (b) {
+                Log.e("widgets", "Barcelona Seçildi")
+            }
+        }
+
+        // v - radioButtonRealMadrid'in kendisi, b - radioButtonRealMadrid'in durumu (true/false)
+        binding.radioButtonRealMadrid.setOnCheckedChangeListener { v, b ->
+            if (b) {
+                Log.e("widgets", "RealMadrid Seçildi")
+            }
+        }
+
         binding.butonGoster.setOnClickListener {
             Log.e("widgets", "Switch durum : ${binding.switch1.isChecked}")
+            Log.e("widgets", "Switch durum : ${binding.checkBoxKotlin.isChecked}")
+            Log.e("widgets", "Switch durum : ${binding.checkBoxJava.isChecked}")
+            Log.e("widgets", "Switch durum : ${binding.radioButtonBarcelona.isChecked}")
+            Log.e("widgets", "Switch durum : ${binding.radioButtonRealMadrid.isChecked}")
         }
     }
 }
