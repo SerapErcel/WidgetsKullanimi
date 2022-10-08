41def8e6618adabb8600e6cc501e7106d2af3ceb
Serap <ercelserap@gmail.com>
2022-10-08 22:21:51 +0300

    Add slider, time and date picker

--- app/src/main/java/com/serapercel/widgetskullanimi/MainActivity.kt
+++ app/src/main/java/com/serapercel/widgetskullanimi/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.serapercel.widgetskullanimi
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.SeekBar
 import com.serapercel.widgetskullanimi.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -68,12 +73,59 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // ProgressBar
+        binding.buttonBasla.setOnClickListener {
+            binding.progressBar.visibility = View.VISIBLE
+        }
+        binding.buttonBitir.setOnClickListener {
+            binding.progressBar.visibility = View.INVISIBLE
+        }
+
+        // Slider
+        binding.slider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                binding.textViewSonuc.text = p1.toString()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+
+        // TimePicker
+        binding.buttonSaat.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val saat = calendar.get(Calendar.HOUR_OF_DAY)
+            val dakika = calendar.get(Calendar.MINUTE)
+
+            val tp = TimePickerDialog(this, { t, s, dk ->
+                binding.textViewZaman.text = "$s : $dk"
+            }, saat, dakika, true)
+            tp.show()
+
+        }
+
+        // DatePicker
+        binding.buttonTarih.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val yil = calendar.get(Calendar.YEAR)
+            val ay = calendar.get(Calendar.MONTH)
+            val gun = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val dp = DatePickerDialog(this, { d, y, a, g ->
+                binding.textViewZaman.text = "$g/${a + 1}/$y"
+            }, yil, ay, gun)
+            dp.show()
+        }
+
         binding.butonGoster.setOnClickListener {
             Log.e("widgets", "Switch durum : ${binding.switch1.isChecked}")
-            Log.e("widgets", "Switch durum : ${binding.checkBoxKotlin.isChecked}")
-            Log.e("widgets", "Switch durum : ${binding.checkBoxJava.isChecked}")
-            Log.e("widgets", "Switch durum : ${binding.radioButtonBarcelona.isChecked}")
-            Log.e("widgets", "Switch durum : ${binding.radioButtonRealMadrid.isChecked}")
+            Log.e("widgets", "Kotlin durum : ${binding.checkBoxKotlin.isChecked}")
+            Log.e("widgets", "Java durum : ${binding.checkBoxJava.isChecked}")
+            Log.e("widgets", "Barcelona durum : ${binding.radioButtonBarcelona.isChecked}")
+            Log.e("widgets", "Real Madrid durum : ${binding.radioButtonRealMadrid.isChecked}")
+            Log.e("widgets", "Slider durum : ${binding.slider.progress}")
+
         }
     }
 }
