d9cf07a11d51e943ac64a341dae4936d2482a1d9
Serap <ercelserap@gmail.com>
2022-10-08 21:24:22 +0300

    Add button, image view and switch

--- app/src/main/java/com/serapercel/widgetskullanimi/MainActivity.kt
+++ app/src/main/java/com/serapercel/widgetskullanimi/MainActivity.kt
@@ -2,6 +2,7 @@ package com.serapercel.widgetskullanimi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.serapercel.widgetskullanimi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Image View
+        binding.buttonResim1.setOnClickListener {
+            binding.imageView.setImageResource(R.drawable.resim1)
+        }
+
+        binding.buttonResim2.setOnClickListener {
+            binding.imageView.setImageResource(
+                resources.getIdentifier(
+                    "resim2",
+                    "drawable",
+                    packageName
+                )
+            )
+        }
+        // v - switch'in kendisi, b - switch'in durumu (true/false)
+        binding.switch1.setOnCheckedChangeListener { v, b ->
+            if (b) {
+                Log.e("widgets", "Switch : ON")
+            } else {
+                Log.e("widgets", "Switch : OFF")
+            }
+        }
+
+        binding.butonGoster.setOnClickListener {
+            Log.e("widgets", "Switch durum : ${binding.switch1.isChecked}")
+        }
     }
 }
